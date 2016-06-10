d4f1b919aeac50c6c795ff03b9cb684ce4404cb5
sudip <soodip.kafle@gmail.com>
2016-06-10 23:59:13 +0545

    secret key

--- config/settings/production.py
+++ config/settings/production.py
@@ -21,7 +21,7 @@ from .common import *  # noqa
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = 'dkfljdkfjslkfjlsdkjfl'
 
 
 # This ensures that Django will be able to detect a secure connection
